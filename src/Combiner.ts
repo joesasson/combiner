a1ace10ef3b432071226edf479bed13d5503cdff
Joe Sasson <ysasson88@gmail.com>
2019-01-04 11:46:09 -0500

    Combine

--- src/Combiner.ts
+++ src/Combiner.ts
@@ -5,19 +5,26 @@ function combineSheets(){
   let folder = folders.next() // first folder with that name
   let files = folder.getFiles()
   let fileIds = []
-  if(files.hasNext()){
+  while(files.hasNext()){
     let file = files.next()
     fileIds.push(file.getId())
   }
   // for each id, get the values contained in the first sheet
-  fileIds.reduce((previous, current) => {
+  let newData = fileIds.reduce((previous, current) => {
     // append them to an array that contains combined values
     let ss = SpreadsheetApp.openById(current)
     let sheet = ss.getSheets()[0] // first sheet, there should only be one sheet
     let data = sheet.getDataRange().getValues()
-    Logger.log({ data })
-    // return previous.concat(data)
+    return previous.concat(data)
   }, [])
   // create a new spreadsheet and set values on first sheet
+  let newSs = SpreadsheetApp.create("Combined")
+
+  createNewSheetWithData(newSs, [newData], "Combined scans")
+  let fileId = newSs.getId()
+  let file = DriveApp.getFileById(fileId)
+  folder.addFile(file)
+  DriveApp.getRootFolder().removeFile(file)
+
 }
 
